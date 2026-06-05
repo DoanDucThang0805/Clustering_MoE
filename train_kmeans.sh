cf85c57686a891eb95c71577442ddac07003c455
DoanDucThang0805 <ducthang8558@gmail.com>
2026-06-05 13:58:37 +0700

    feat: Update training script to include additional seeds and cluster configurations for non-pretrained backbone

--- train_kmeans.sh
+++ train_kmeans.sh
@@ -2,6 +2,19 @@
 
 source venv/bin/activate
 cd src
+python -m training.kmean \
+    --dataset_name "plantdoc" \
+    --backbone_type "non_pretrain_backbone" \
+    --backbone_name "mobilenetv3small_torchvision" \
+    --seed 42 \
+    --num_clusters 2 3 4 5 6 8
+
+python -m training.kmean \
+    --dataset_name "plantdoc" \
+    --backbone_type "non_pretrain_backbone" \
+    --backbone_name "mobilenetv3small_torchvision" \
+    --seed 43 \
+    --num_clusters 2 3 4 5 6 8
 
 python -m training.kmean \
     --dataset_name "plantdoc" \
